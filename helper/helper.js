5ba30338cef474cb0197d42db4a9daefeaeb9531
Aaron Primmer <aaronmprimmer@gmail.com>
2026-01-14 17:54:30 -0500

    Add note working

--- helper/helper.js
+++ helper/helper.js
@@ -2,27 +2,32 @@ const fs = require("fs");
 const path = require("path");
 const db = require("../db/db.json");
 
-function addNote(noteToAdd){
-    db.push(noteToAdd);
-    fs.writeFileSync(path.join(__dirname, "..", "db", "db.json"), db, (error) => {
-        console.log("Error Writing File: ", error);
-    });
+function addNote(noteToAdd) {
+  console.log("Adding Note: ", noteToAdd);
+  db.push(noteToAdd);
+  fs.writeFileSync(
+    path.join(__dirname, "..", "db", "db.json"),
+    JSON.stringify(db, null, 2),
+    (error) => {
+      console.log("Error Writing File: ", error);
+    }
+  );
 }
 
-function uniqueID(){
-    let foundID = "";
-    const id = Math.random().toString(36).substring(2) + Date.now().toString(36);
-    const foundItem = db.find(item => {if(item.id === id){
-        foundID = item.id;
-    }});
-
-    if(!foundID){
-        return id;
-    }else{
-        return uniqueID();
+function uniqueID() {
+  let foundID = "";
+  const id = Math.random().toString(36).substring(2) + Date.now().toString(36);
+  const foundItem = db.find((item) => {
+    if (item.id === id) {
+      foundID = item.id;
     }
+  });
 
-    
+  if (!foundID) {
+    return id;
+  } else {
+    return uniqueID();
+  }
 }
 
 module.exports = { addNote, uniqueID };
--- public/assets/js/notesScript.js
+++ public/assets/js/notesScript.js
@@ -1,20 +1,52 @@
 let saveButtonPressed = false;
 const saveButton = document.getElementById("saveButton");
+const saveNoteForm = document.getElementById("saveNoteForm");
 const cancelNoteButton = document.getElementById("cancelNoteButton");
+const titleField = document.getElementById("title");
+const textField = document.getElementById("text");
 
-saveButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    if(saveButtonPressed){
-        return;
-    }
-    saveButtonPressed = true;
-    console.log("Save button pressed");
+saveNoteForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  if (saveButtonPressed) {
+    return;
+  }
+  saveButtonPressed = true;
+
+  console.log(event.target);
+  console.log("Save button pressed");
+  if (titleField.value.trim() !== "" && textField.value.trim() !== "") {
+    const formData = new FormData(event.target);
+    console.log("Form Data:", ...formData.entries());
+    const data = Object.fromEntries(formData.entries());
+    console.log("Data to be sent:", data);
+    fetch("/api/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.success) {
+          window.location.replace("/");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  } else {
     saveButtonPressed = false;
+    return;
+  }
+  saveButtonPressed = false;
 });
 
 cancelNoteButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    window.location.replace("/");
+  event.preventDefault();
+  event.stopPropagation();
+  window.location.replace("/");
 });
